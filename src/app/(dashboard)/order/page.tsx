67a389a6453d4073b4b1dd722bac0d4850d52f1e
RaihanFadhilB <tgun8113@gmail.com>
2025-09-17 18:56:40 +0700

    feat: Order Management

---
+++ src/app/(dashboard)/order/page.tsx
@@ -0,0 +1,9 @@
+import OrderManagement from "./_components/order";
+
+export const metadata = {
+  title: "MRJ Snack | Order Management",
+};
+
+export default function OrderManagementPage() {
+  return <OrderManagement />;
+}
